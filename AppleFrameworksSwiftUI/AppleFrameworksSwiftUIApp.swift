9f8a474190fc4fe084449887de7eafe8608331b4
Ali Shaker <ali.s@opensooq.com>
2023-11-20 12:22:59 +0300

    Initial Commit

---
+++ AppleFrameworksSwiftUI/AppleFrameworksSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksSwiftUIApp.swift
+//  AppleFrameworksSwiftUI
+//
+//  Created by Ali Shaker on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
